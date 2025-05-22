2e83da274a48b8fd9ed60bce3be03bf212b70063
Krzysztof Rusek <krusek@agh.edu.pl>
2025-05-22 18:11:58 +0200

    Po lab - kalsa do przechwytywania stdout stderr

--- nbconf.py
+++ nbconf.py
@@ -3,9 +3,16 @@ c.NbConvertApp.export_format = 'markdown'
 
 c.TemplateExporter.exclude_input = True
 c.TemplateExporter.exclude_input_prompt = True
-c.TemplateExporter.exclude_output_prompt = True
+# c.TemplateExporter.exclude_output_prompt = True
 c.ExecutePreprocessor.enabled = True
 c.TagRemovePreprocessor.enabled = True
 c.TagRemovePreprocessor.remove_cell_tags.add("hide")
+c.TagRemovePreprocessor.remove_all_outputs_tags.add("tf")
 # c.NbConvertApp.output_base='./out/{notebook_name}'
 c.NbConvertApp.output_files_dir = 'img'
+
+# c.Exporter.preprocessors = [
+#     'nbconvert.preprocessors.ExecutePreprocessor',
+#     'nbconvert.preprocessors.TagRemovePreprocessor'
+#     # Add other preprocessors here if needed, in desired order
+# ]
