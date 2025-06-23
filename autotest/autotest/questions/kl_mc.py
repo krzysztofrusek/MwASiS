301c2bfa91cf461efea7d2f46d1289a9756a0cb9
Krzysztof Rusek <krusek@agh.edu.pl>
2025-06-23 23:37:03 +0200

    Update KL divergence calculation to use Laplace distribution

--- autotest/autotest/questions/kl_mc.py
+++ autotest/autotest/questions/kl_mc.py
@@ -12,8 +12,8 @@ def kl(seed: int, info: QuestionInfo = None) -> QuestionInfo:
 
 Wyznaczyć numerycznie dywergencję Kullbacka-Leiblera `KL(q|p)` pomiędzy
 
-- `q(x)  = N(mu, sigma^2)` i
-- `p(x) = Exp(lam)`
+- `q(x)  = N(mu, sigma)` i
+- `p(x) = Laplace(mu,sigma)`
 
 __Obliczenia wykonać dla min 50000 punktów__
 
@@ -21,31 +21,30 @@ __Obliczenia wykonać dla min 50000 punktów__
 
 - `mu = {{mu}}`
 - `sigma = {{sigma}}`
-- `lam = {{lam}}`
+
 
 
 """
 
     N=100000
     qfun = lambda mu, sigma: tfd.Sample(tfd.Normal(loc=mu, scale=sigma), N)
-    pfun = lambda lam: tfd.Sample(tfd.Exponential(rate=lam), N)
+    pfun = lambda mu, sigma: tfd.Sample(tfd.Laplace(loc=mu, scale=sigma), N)
 
     joint = tfd.JointDistributionNamed(dict(
         mu = tfd.Normal(loc=0.0, scale=1.0),
         sigma = tfd.Exponential(rate=1),
-        lam = tfd.Exponential(rate=1),
         q = qfun,
         # p = pfun
     ))
 
     s = joint.sample(seed=seed)
     q = qfun(s['mu'], s['sigma'])
-    p = pfun(s['lam'])
+    p = pfun(s['mu'], s['sigma'])
     kl = (q.log_prob(s['q']) - p.log_prob(s['q']))/N
 
     mu = float(s['mu'].numpy())
     sigma = float(s['sigma'].numpy())
-    lam = float(s['lam'].numpy())
+
     answer = float(kl.numpy())
     tolerance = 0.1
     ...
